1840428ec7738aec77ecab08d4f2f2b26b7455b3
scmmishra <scm.mymail@gmail.com>
2018-09-28 15:12:42 +0530

    Removed unused files

--- academy/www/classrooms/expert-user.js
+++
@@ -1,9 +0,0 @@
-// $('.card-clickable').on('click', function(event) {
-//     $variable = $(this).data("list");
-// 	var x = document.getElementById($variable)
-// 	if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// });
