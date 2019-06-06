a8506d428e69f6fdeb0d07602d0f0d614e9be580
Shivam Mishra <scm.mymail@gmail.com>
2019-06-06 20:11:25 +0530

    refactor: added default title for academy homepage

--- academy/www/index.py
+++ academy/www/index.py
@@ -2,4 +2,4 @@ from __future__ import unicode_literals
 import frappe
 
 def get_context(context):
-	context.academy = frappe.get_doc("Academy Settings")
+	context.academy = frappe.get_single("Academy Settings")
